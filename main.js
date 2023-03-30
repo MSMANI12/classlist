65b6fbbcada73b9d07aa32430505ed9f520aec3f
MSMANI12 <selvamani12121998@gmail.com>
2023-03-30 14:11:33 +0530

    add classlist

---
+++ main.js
@@ -0,0 +1,24 @@
+function on(){
+	document.getElementById("h").classList.remove("c1")
+	document.getElementById("h").classList.remove("c2")
+	
+}
+
+function on1(){
+	document.getElementById("he").classList.toggle("c3")
+}
+
+function on2(){
+	document.getElementById("he1").classList.add("c3")
+	document.getElementById("he1").classList.add("c2")
+}
+
+function onimg(){
+	document.getElementById("im").classList.toggle("c5")
+}
+// function myFunction(){
+	// const element=document.getElementById("myDIV");
+	
+		// element.classList.toggle("mystyle");
+	
+// }
